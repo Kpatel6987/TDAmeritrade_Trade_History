a5078f62af73c291d289cf05254dcf356c4972e6
Kavish Patel <kpatel6987@gmail.com>
2017-08-16 23:35:52 -0400

    legs based off just symbol as the key

--- trade_history.py
+++ trade_history.py
@@ -1,3 +1,5 @@
+import json
+
 '''
 ,Exec Time,Spread,Side,Qty,Pos Effect,Symbol,Exp,Strike,Type,Price,Net Price,Order Type
 
@@ -23,23 +25,54 @@
 
 def build_position_dictionary(order, legs):
     return {
-        "spread": order[2],
-        "side": order[3],
-        "quantity": order[4],
-        "net_price": order[11],
-        "legs": legs
+        "pl": 0,
+        "commission": 0,
+        "total_pl": 0,
+        "legs": legs,
+        "num_legs": len(legs)
     }
 
-def build_leg_dictionary(order):
+def build_leg_dictionary(order, base_order):
     return {
-        "quantity": order[4],
-        "expiration": order[7],
+        "date": base_order[1],
+        "spread": base_order[2],
         "side": order[3],
+        "quantity": order[4],
+        "position_effect": order[5],
+        "expiration": order[7],
         "strike": order[8],
-        "price": order[10],
-        "option_type": order[9]
+        "option_type": order[9],
+        "price": order[10]
     }
 
+def handle_two_legs(order, legs, lines, i):
+    new_order = lines[i].split(",")
+    legs.append(build_leg_dictionary(new_order, order))
+
+def handle_four_legs(order, legs, lines, i):
+    for x in range(1, 4):
+        new_order = lines[i + x].split(",")
+        legs.append(build_leg_dictionary(new_order, order))
+
+def add_position(order, key, legs):
+    if key in positions.keys():
+        old_legs = positions[key]["legs"]
+        legs += old_legs
+        positions[key]["legs"] = legs
+    else:
+        value = build_position_dictionary(order, legs)
+        positions[key] = value
+    update_pl(order, key, legs)
+
+def update_pl(order, key, legs):
+    pl = 0.0
+    commission = 0.0
+    for x in legs:
+        pl += (float(x["price"]) * float(x["quantity"]) * -1)
+        commission += .015 * abs(float(x["quantity"]))
+    positions[key]["total_pl"] = "{0:.2f}".format((pl - commission) * 100)
+    positions[key]["pl"] = "{0:.2f}".format(pl * 100)
+    positions[key]["commission"] = "{0:.2f}".format(commission * 100)
 
 def parse_file(lines):
     check = False
@@ -51,42 +84,26 @@ def parse_file(lines):
             return positions
         if check:
             order = lines[i].split(",")
-            key = (order[6], order[2], order[7])
-            # position effect
-            if order[5] == "TO CLOSE" and key in positions.keys():
-                print(positions[key])
-            else:
-                legs = [build_leg_dictionary(order)]
-                # spread
-                if order[2] == "VERTICAL":
-                    i += 1
-                    new_order = lines[i].split(",")
-                    # check if debit or credit
-                    if new_order[11] == "DEBIT":
-                        order[11] = str(float(order[11]) * -1)
-                    legs.append(build_leg_dictionary(new_order))
-                elif order[2] == "IRON CONDOR" or order[2] == "VERT ROLL":
-                    for x in range(1, 4):
-                        new_order = lines[i + x].split(",")
-                        if x == 1 and new_order[11] == "DEBIT":
-                            order[11] = str(float(order[11]) * -1)
-                        # position effect
-                        if new_order[5] == "TO CLOSE" and key in positions.keys():
-                            # net price
-                            positions[key]["net_price"] =
-                                str(float(positions[key]["net_price"]) + float(order[11]))
-                        legs.append(build_leg_dictionary(new_order))
-                    i += 3
-                value = build_position_dictionary(order, legs)
-                positions[key] = value
+            # symbol
+            key = order[6]
+            legs = [build_leg_dictionary(order, order)]
+            if order[2] == "VERTICAL":
+                i += 1
+                handle_two_legs(order, legs, lines, i)
+            elif order[2] == "IRON CONDOR" or order[2] == "VERT ROLL":
+                handle_four_legs(order, legs, lines, i)
+                i += 3
+            add_position(order, key, legs)
         i += 1
 
 def main():
     file = open("2017-08-15-AccountStatement.csv")
     lines = file.readlines()
     file.close()
-    parse_file(lines)
-    #print(parse_file(lines))
+    pos = parse_file(lines)
+    print(json.dumps(pos, indent=4))
+
+
 
 if __name__ == "__main__":
     positions = {}
