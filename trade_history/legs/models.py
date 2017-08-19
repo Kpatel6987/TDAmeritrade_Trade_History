023c43183172ad23437ddac6bed5e46be7909f77
Kavish Patel <kpatel6987@gmail.com>
2017-08-19 00:50:38 -0400

    fixed model for legs

--- trade_history/legs/models.py
+++ trade_history/legs/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 class Leg(models.Model):
+    SIDE_CHOICES = (("BUY", "BUY"), ("SELL", "SELL"))
+    POSITION_CHOICES = (("TO OPEN", "TO OPEN"), ("TO CLOSE", "TO CLOSE"))
+    OPTION_CHOICES = (("CALL", "CALL"), ("PUT", "PUT"))
     datetime = models.CharField(max_length=100)
     spread = models.CharField(max_length=100)
-    side = models.ChoiceField()
+    side = models.CharField(choices=SIDE_CHOICES)
     quantity = models.IntegerField()
-    position_effect = models.ChoiceField()
+    position_effect = models.CharField(choicecs=POSITION_CHOICES)
     expiration = models.CharField(max_length=100)
-    strike = models.IntegerField()
-    option_type = models.ChoiceField()
-    price = models.IntegerField()
+    strike = models.DecimalField()
+    option_type = models.CharField(choices=OPTION_CHOICES)
+    price = models.DecimalField()
 
     def __str__(self):
         return self.pk
